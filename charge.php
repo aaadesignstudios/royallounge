942b4023401b9c4068132e29c439852c76c4d454
Antonio Allen <aaadesignstudios@gmail.com>
2015-09-25 15:55:12 -0400

    Stripe

--- charge.php
+++ charge.php
@@ -1,5 +1,5 @@
 <?php
-  require_once('./lib/config.php');
+  require_once('./config.php');
 
   $token  = $_POST['stripeToken'];
 
--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-require_once('./lib/Stripe.php');
+require_once('vendor/autoload.php');
 
 $stripe = array(
   "secret_key"      => "sk_test_D5SturdPe6H6f0UUTJNXrv5N",
